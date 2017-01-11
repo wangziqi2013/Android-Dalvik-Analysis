2026a9e8b7d7f57ffa31d9b084708e9d420cc1cd
Ziqi Wang <675973885@qq.com>
2017-01-10 22:29:20 -0500

    Adding file header read test

---
+++ test/apk_test.cpp
@@ -0,0 +1,15 @@
+
+/*
+ * apk_test.cpp - This file tests basic APK operation
+ */ 
+ 
+/*
+ * TestReadFileHeader() - Tests whether file header could be read
+ */
+void TestReadFileHeader() {
+  
+}
+
+int main() {
+  return 0; 
+}
