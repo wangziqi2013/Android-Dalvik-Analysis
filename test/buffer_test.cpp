512365453b49c8bdd18aa3367f4cbcb7dea7495e
Ziqi Wang <675973885@qq.com>
2017-01-16 16:20:31 -0500

    Making test work

--- test/buffer_test.cpp
+++ test/buffer_test.cpp
@@ -6,6 +6,9 @@
 #include "test_suite.h"
 #include "common.h" 
 
+using namespace wangziqi2013;
+using namespace android_dalvik_analysis;
+
 static const char *test_file_name = "buffer_test.temp";
 
 /*
@@ -16,14 +19,14 @@ void TestBasicExtend() {
   
   // Declear a buffer of size 1
   Buffer buffer{1};
-  assert(buffer.GetLength() == 1UL);
+  assert(buffer.GetLength() == 0UL);
   
   // Write 1 byte data into it - this does not cause extend
   buffer.AppendByte('a');
   assert(buffer.GetLength() == 1UL);
   
   // Append data and '\0' to the buffer
-  char *data = "0123456789abcdefghijklmnopqrstuvwxyz";
+  const char *data = "0123456789abcdefghijklmnopqrstuvwxyz";
   size_t data_len = strlen(data);
   
   buffer.Append(data, data_len + 1);
@@ -38,11 +41,16 @@ void TestBasicExtend() {
   
   fclose(fp);
   
-  char *s = FileUtility::LoadFile(test_file_name);
+  char *s = reinterpret_cast<char *>(FileUtility::LoadFile(test_file_name));
+  // ISO C++ forbids converting a string constant to ¡®char*¡¯
+  const char *result = "a0123456789abcdefghijklmnopqrstuvwxyz";
   
-  assert(strcmp("a0123456789abcdefghijklmnopqrstuvwxyz", s) == 0);
+  assert(strcmp(result, s) == 0);
   
   delete[] s;
+  
+  // Delete the temp file for testing
+  unlink(test_file_name);
   
   return;
 }
