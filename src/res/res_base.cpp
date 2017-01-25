6f74b75a7fd1cce4f3087b1f9b77b6cf047f8329
Ziqi Wang <675973885@qq.com>
2017-01-25 17:42:28 -0500

    Adding cpp file for declaring global variables

---
+++ src/res/res_base.cpp
@@ -0,0 +1,16 @@
+
+#include "res_base.h"
+
+namespace wangziqi2013 {
+namespace android_dalvik_analysis {
+
+// This is the header line of any valid XML file
+const char ResourceBase::XML_HEADER_LINE[] = \
+  "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n";
+  
+const char ResourceBase::RESOURCE_END_TAG[] = "</resource>\n";
+
+} // namespace android_dalvik_analysis
+} // namespace wangziqi2013
+
+
