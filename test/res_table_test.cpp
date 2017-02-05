1a4153d9df618ab8755883cbedcbc4a2dfcbcef6
Ziqi Wang <675973885@qq.com>
2017-02-04 22:06:06 -0500

    Fixed a memory problem and test case now correctly unzips XML files for facebook

--- test/res_table_test.cpp
+++ test/res_table_test.cpp
@@ -21,6 +21,9 @@ void TestResourceTableBasic(const char *file_name) {
   size_t length;
   unsigned char *data = FileUtility::MapFileReadOnly(file_name, &length);
   
+  // This will be destroyed after this function exits
+  // and resource table automatically deregisters itself from the
+  // package group map
   ResourceTable table{data, length, false};
   
   fprintf(stderr, "\n========================================\n");
@@ -45,20 +48,23 @@ void TestResourceTableBasic(const char *file_name) {
  *                        some app might refer to the resource ID in the systems
  *                        resource file 
  */
-unsigned char *LoadSystemResource(size_t *length_p) {
+unsigned char *LoadSystemResource(size_t *length_p, 
+                                  ResourceTable **table_p_p) {
   _PrintTestName();
   
   size_t length;
   unsigned char *data = \
     FileUtility::MapFileReadOnly("resources-system.arsc", &length);
   
-  ResourceTable table{data, length, false};
+  // This must be on the heap to avoid overwriting
+  ResourceTable *table_p = new ResourceTable{data, length, false};
   
   dbg_printf("Writing XML for Android system resource file\n");
-  table.DebugWriteXml();
+  table_p->DebugWriteXml();
   
   // Return the length for later freeing this
   *length_p = length;
+  *table_p_p = table_p;
   
   return data;
 }
@@ -68,7 +74,11 @@ int main(int argc, char **argv) {
   
   dbg_printf("Opening Android system resource file\n");
   size_t sys_res_length;
-  unsigned char *sys_res_data_p = LoadSystemResource(&sys_res_length);
+  ResourceTable *table_p = nullptr;
+  
+  unsigned char *sys_res_data_p = \
+    LoadSystemResource(&sys_res_length, &table_p);
+    
   dbg_printf("Finished opening Android system resource file\n");
   
   Argv args{argc, argv};
@@ -81,6 +91,8 @@ int main(int argc, char **argv) {
     assert(false && "Wrong argument list - too many input files!");
   }
   
+  // This does not try to free the mapped file region
+  delete table_p;
   FileUtility::UnmapFile(sys_res_data_p, sys_res_length); 
   
   return 0; 
