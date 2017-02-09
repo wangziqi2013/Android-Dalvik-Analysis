9415fc233f02236631acbd73f5aea77671f96230
Ziqi Wang <675973885@qq.com>
2017-02-09 00:38:58 -0500

    Adding more test cases for 3, 4, 5 byte ULEB case

--- test/encoding_test.cpp
+++ test/encoding_test.cpp
@@ -26,6 +26,33 @@ void TestULEB128() {
   assert(value1 == value2);
   assert(p1 == p2);
   
+  uint8_t test2[] = {0x88, 0x97, 0x7F};
+  
+  p1 = p2 = test2;
+  value1 = EncodingUtility::ReadULEB128(&p1);
+  value2 = EncodingUtility::ReadLEB128(&p2, false);
+  dbg_printf("v1 = %u; v2 = %u\n", value1, value2);
+  assert(value1 == value2);
+  assert(p1 == p2);
+  
+  uint8_t test3[] = {0x88, 0x97, 0xAF, 0x03};
+  
+  p1 = p2 = test3;
+  value1 = EncodingUtility::ReadULEB128(&p1);
+  value2 = EncodingUtility::ReadLEB128(&p2, false);
+  dbg_printf("v1 = %u; v2 = %u\n", value1, value2);
+  assert(value1 == value2);
+  assert(p1 == p2);
+  
+  uint8_t test4[] = {0x88, 0x97, 0xAF, 0xF3, 0x01};
+  
+  p1 = p2 = test4;
+  value1 = EncodingUtility::ReadULEB128(&p1);
+  value2 = EncodingUtility::ReadLEB128(&p2, false);
+  dbg_printf("v1 = %u; v2 = %u\n", value1, value2);
+  assert(value1 == value2);
+  assert(p1 == p2);
+  
   return;
 }
 
